f5fed1933051e275fe826fcc9c4ee16c74c43a55
Nico.D <nicolas.drew@hotmail.com>
2025-08-05 10:55:29 +0200

    feat: Import AdminPage component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import AdminPage from "./pages/AdminPage";
 import Footer from "./components/Footer";
 
 const App = () => {
