d5ad845dc378f91429a2e1e20b675ed948011e03
IDGHIM <ichemdghim@gmail.com>
2025-08-05 10:39:50 +0200

    Admin Panel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from './pages/LoginPage';
+import AdminPage from './pages/AdminPage'; 
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         {/*page 404 */}
         <Route path="*" element={<div>Page non trouvée</div>} />
       </Routes>
