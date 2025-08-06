ddb675f31078ac41b7bf580f6f7bf0a3c3f6907c
IDGHIM <ichemdghim@gmail.com>
2025-08-06 09:40:58 +0200

    Merge App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-
-=======
->>>>>>> Ichem
 import Navbar from "./components/Navbar"; 
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
