9e8ef5c25d5dc15f3f8a366c4ec3fe1b0c0cbb7d
IDGHIM <ichemdghim@gmail.com>
2025-08-06 09:39:46 +0200

    Package Axios / Page Forgot Reset Password

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage"; 
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 import AdminPage from "./pages/AdminPage";
 import QuizPage from "./pages/QuizPage";
 import ContactPage from "./pages/ContactPage"; 
@@ -19,6 +21,9 @@ const App = () => {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password:token" element={<ResetPasswordPage />} />
+        {/* Admin route */}
         <Route path="/admin" element={<AdminPage />} />
         {/* page 404 */}
         <Route path="*" element={<div>Page non trouvée</div>} />
