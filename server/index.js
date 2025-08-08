b930e846b19f227bc57e1abf82d74f6fdcd72e10
Damien Di Luca <damiendiluca@MacBook-Air-de-Damien.local>
2025-08-08 10:33:50 +0200

    Résolution PORT back

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
